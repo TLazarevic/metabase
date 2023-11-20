208f1f040be3960d26365bf975a2312db9dcc4fe
Nick Fitzpatrick <nickfitz.582@gmail.com>
2023-11-20 17:42:33 -0400

    rough table selection. dont fire me plz

--- frontend/src/metabase/home/components/HomePage/HomePage.tsx
+++ frontend/src/metabase/home/components/HomePage/HomePage.tsx
@@ -14,7 +14,7 @@ import { canUseMetabotOnDatabase } from "metabase/metabot/utils";
 import { addUndo } from "metabase/redux/undo";
 import { getSettingsLoading } from "metabase/selectors/settings";
 
-// import { QuestionPicker } from "metabase/components/EntityPicker"; // FIXME
+// import { TablePicker } from "metabase/components/EntityPicker"; // FIXME
 
 import type Database from "metabase-lib/metadata/Database";
 import type { CollectionItem, DashboardId } from "metabase-types/api";
@@ -49,7 +49,7 @@ export const HomePage = (): JSX.Element => {
 
   return (
     <HomeLayout hasMetabot={getHasMetabot(databases, models, isMetabotEnabled)}>
-      {/* <QuestionPicker onItemSelect={console.log} /> */}
+      {/* <TablePicker onItemSelect={console.log} /> */}
       <HomeContent />
     </HomeLayout>
   );
